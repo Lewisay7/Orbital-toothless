795efb71b2dc87ce813ac0d330bfb5f600ca62ef
Lewis Chong Li Wei <e0902029@u.nus.edu>
2022-07-26 17:08:08 +0800

    -

--- mainpage.php
+++ mainpage.php
@@ -16,7 +16,7 @@ $row_check=mysqli_fetch_assoc($result_check);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href='https://fonts.googleapis.com/css?family=Oswald:700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
-    <link rel="stylesheet" href="./design.css">
+    <link rel="stylesheet" href="css/design.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="fonts/icomoon/style.css">
