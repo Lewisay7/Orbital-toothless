43c20478c7782788062ba4ea62a8f019da1db65a
Lewis Chong Li Wei <e0902029@u.nus.edu>
2022-08-12 11:27:03 +0800

    removed database config php file

--- db_configuration.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="calstrack";
-$conn=mysqli_connect($servername,$username,$password,$dbname);
-if(!$conn){
-    echo "Database Connection Failed.";}
-?>
